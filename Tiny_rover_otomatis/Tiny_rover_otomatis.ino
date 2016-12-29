e7484a4d0457eeb1a35b411dc59694a40e836209
irulraspi <irulasslab@gmail.com>
2016-12-29 13:38:40 +0700

    Add files via upload

---
+++ Tiny_rover_otomatis/Tiny_rover_otomatis.ino
@@ -0,0 +1,14 @@
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // inisialisasi pin untuk Tiny85
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(0, HIGH);
+  digitalWrite(1, LOW);   // turn the LED on (HIGH is the voltage level)
+  }
